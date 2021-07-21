8d072b1daf5f4b1c1f010f60b9830e7b1ffd2800
fcd <git@fcd.dev>
2021-07-21 12:02:39 +0100

    check if agent has any identities

--- geheim.sh
+++ geheim.sh
@@ -2,6 +2,12 @@
 set -euEo pipefail
 
 function geheim() {
+    if ! ssh-add -l > /dev/null 2>&1
+    then
+        echo 'SSH agent has no identities'
+        exit 1
+    fi
+
     root_dir=$(dirname $(realpath $0))
     pushd "${root_dir}/terraform/ephemeral"
 
