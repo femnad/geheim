957e9fadff55fcebdc6630914be247f95f6a8191
fcd <git@fcd.dev>
2021-11-18 18:25:55 +0000

    get public IP if no IP has been provided

--- geheim-guest.sh
+++ geheim-guest.sh
@@ -4,13 +4,18 @@ set -euEo pipefail
 function geheim-guest() {
     rp="$(realpath $0)"
 
-    if [ $# -ne 1 ]
+    if [ $# -gt 1 ]
     then
-        echo "Usage: $(basename $rp) <ip>"
+        echo "Usage: $(basename $rp) [ip]"
         exit 1
     fi
 
-    ip=$1
+    if [ $# -ne 1 ]
+    then
+        ip=$(curl -sS ipinfo.io/json | jq -r .ip)
+    else
+        ip=$1
+    fi
 
     root_dir=$(dirname $rp)
     pushd "${root_dir}/terraform/ephemeral/guest"
